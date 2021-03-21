5b603dd798a0556d1402d8002ef142eddc297539
xueyueli <xueyueli@tencent.com>
2021-03-21 08:29:13 +0800

    完善

--- web/src/components/card/DeskCard.vue
+++ web/src/components/card/DeskCard.vue
@@ -1,14 +1,18 @@
 <!-- eslint-disable -->
 <template>
+
+
   <el-card :body-style="{ padding: '0px' }" shadow="hover" style="position:relative;">
+    <el-badge class="item" :hidden="desk===undefined || desk.id===0" value="已占用" style="position: absolute;right: 10px;top:5px">
+    </el-badge>
     <img style="height:200px;width:200px; border: none" :src="'api/file/' + desk.space.pictureStorePath"
          v-if="desk.space!==undefined">
     <div
-      style="width: 200px;height: 30px;position:relative;padding: 0;margin: 0; border: none;"
-      :class="{'isOpening':desk.id!==0}">
+      style="width: 200px;height: 30px;position:relative;padding: 0;margin: 0; border: none;">
       <span style="position:absolute; left:5px" v-if="desk.space!==undefined">{{ desk.space.name }}</span>
     </div>
   </el-card>
+
 </template>
 
 <script>
